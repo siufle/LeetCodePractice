b316b895caeafb2a19c1264ef850c46276c81c8d
huang <3389254540@qq.com>
2025-09-03 15:51:45 +0800

    3.无重复子串

--- src/slideWindow/NO3.java
+++ src/slideWindow/NO3.java
@@ -1,4 +1,31 @@
 package slideWindow;
 
 public class NO3 {
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        Solution solution = new Solution();
+        int i = solution.lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        char[] chs = s.toCharArray();
+        int n = chs.length;
+        int ans = 0;
+        int left = 0;
+        int[] cnt = new int[128];
+        for(int right = 0; right < n; right++){
+            char c = chs[right];
+            cnt[c]++;
+            while(cnt[c] > 1){
+                cnt[chs[left]]--;
+                left++;
+            }
+            ans = Math.max(ans,right - left + 1);
+        }
+        return ans;
+    }
 }
