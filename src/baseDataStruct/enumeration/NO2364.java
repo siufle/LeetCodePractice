1cf2930781ca55da2685bbc15fe6e7739def4e30
huang <3389254540@qq.com>
2026-03-06 20:14:17 +0800

    2364. 统计坏数对的数目

---
+++ src/baseDataStruct/enumeration/NO2364.java
@@ -0,0 +1,54 @@
+/*2364. 统计坏数对的数目
+给你一个下标从 0 开始的整数数组 nums 。
+如果 i < j 且 j - i != nums[j] - nums[i] ，那么我们称 (i, j) 是一个 坏数对 。
+请你返回 nums 中 坏数对 的总数目。
+
+示例 1：
+输入：nums = [4,1,3,3]
+输出：5
+解释：数对 (0, 1) 是坏数对，因为 1 - 0 != 1 - 4 。
+数对 (0, 2) 是坏数对，因为 2 - 0 != 3 - 4, 2 != -1 。
+数对 (0, 3) 是坏数对，因为 3 - 0 != 3 - 4, 3 != -1 。
+数对 (1, 2) 是坏数对，因为 2 - 1 != 3 - 1, 1 != 2 。
+数对 (2, 3) 是坏数对，因为 3 - 2 != 3 - 3, 1 != 0 。
+总共有 5 个坏数对，所以我们返回 5 。
+
+示例 2：
+输入：nums = [1,2,3,4,5]
+输出：0
+解释：没有坏数对。
+
+提示：
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+*/
+
+package baseDataStruct.enumeration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NO2364 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2,3,4,5};
+        long res = solution.countBadPairs(nums);
+        System.out.println(res);
+    }
+    static class Solution {
+        public long countBadPairs(int[] nums) {
+            for(int i = 0; i < nums.length; i++){
+                nums[i] -= i;
+            }
+            long ans = 0L;
+            Map<Long, Long> map = new HashMap<>();
+            for(int i = 0; i < nums.length; i++){
+                long temp = nums[i];
+                long cnt = map.getOrDefault(temp, 0L);
+                ans += i - cnt;
+                map.put(temp, cnt + 1);
+            }
+            return ans;
+        }
+    }
+}
